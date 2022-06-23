50679947f483d636cffc1253aa5f35cc12624541
Abhitator <89402434+Abhitator216@users.noreply.github.com>
2022-06-23 21:21:36 +0530

    Update B_Queue_at_the_School.cpp

--- B_Queue_at_the_School/B_Queue_at_the_School.cpp
+++ B_Queue_at_the_School/B_Queue_at_the_School.cpp
@@ -1,30 +1,27 @@
-#include<bits/stdc++.h>
-#define ll long long int 
-#define cy cout<<"YES\n";
-#define cn cout<<"NO\n";
+#include <bits/stdc++.h>
+#define ll long long int
+#define cy cout << "YES\n";
+#define cn cout << "NO\n";
 #define mod 1000000007
 using namespace std;
 int main()
 {
-    string k="",s;
-    cin >> s;
-    for (int i = 0; i < s.size(); i++)
+    int n, t;
+    cin >> n >> t;
+    string s;
+    cin>>s;
+    while (t--)
     {
-        if (s[i]=='.')
+
+        for (int i = 0; i < s.size()-1; i++)
         {
-            k+="0";
-        }else{
-            if (s[i+1]=='.')
+            if (s[i]=='B'&&s[i+1]=='G')
             {
-                k+="1";
-            }else{
-                k+="2";
+                swap(s[i],s[i+1]);
             }
-            i++;
+            
         }
-        
     }
-    
-cout<<k;
+    cout<<s;
     return 0;
 }
