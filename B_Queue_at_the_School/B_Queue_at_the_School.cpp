a1471bc8767590e0e72288232f1ffb066000efd4
Abhitator <89402434+Abhitator216@users.noreply.github.com>
2022-06-23 20:53:03 +0530

    B_Queue_at_the_School

---
+++ B_Queue_at_the_School/B_Queue_at_the_School.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define cy cout << "YES\n";
+#define cn cout << "NO\n";
+#define mod 1000000007
+using namespace std;
+int main()
+{
+    int n, t;
+    cin >> n >> t;
+    string s;
+    cin>>s;
+    while (t--)
+    {
+
+        for (int i = 0; i < s.size()-1; i++)
+        {
+            if (s[i]=='B'&&s[i+1]=='G')
+            {
+                swap(s[i],s[i+1]);
+            }
+            
+        }
+    }
+    cout<<s;
+    return 0;
+}
